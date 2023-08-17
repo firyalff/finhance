945df217f42f2eec869a449f581888830084336b
Firyal Fakhrilhadi <firyal@sampingan.co.id>
2023-08-17 14:49:46 +0700

    feat(): move cashflow type to category from cashflows

--- app/finhancesvc/dbmigration/sql/20230801061207_cashflow_category.up.sql
+++ app/finhancesvc/dbmigration/sql/20230801061207_cashflow_category.up.sql
@@ -1,9 +1,11 @@
 BEGIN;
 
+CREATE TYPE CASHFLOW_CATEGORY_TYPE_ENUM AS ENUM ('income', 'expense');
+
 CREATE TABLE cashflow_categories (
     id uuid PRIMARY KEY,
     user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    cashflow_category_type CASHFLOW_TYPE_ENUM NOT NULL,
+    cashflow_category_type CASHFLOW_CATEGORY_TYPE_ENUM NOT NULL,
     name TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NULL,
